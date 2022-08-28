a5f2d81fd89b894c0a9f40dd0ab3a4d63cee0501
leonardo mosso <leonardo_mcv@hotmail.com>
2022-08-28 12:28:06 -0300

    commit changes

--- 01-birthday-reminder/setup/src/App.js
+++ 01-birthday-reminder/setup/src/App.js
@@ -2,7 +2,37 @@ import React, { useState } from 'react';
 import data from './data';
 import List from './List';
 function App() {
-  return <h2>reminder project setup</h2>;
+
+  const [people, setPeople] = useState(data)
+  console.log(people)
+
+  function cleanAll(){
+    setPeople([])
+
+  };
+
+  function refreshPage() {
+    window.location.reload(false);
+  };
+
+
+  return (
+  
+  <main>
+
+      <section className='container'>
+
+        <h3>{people.length} birthdays today</h3>
+        <List people={people} />
+        <button onClick={cleanAll}>Clear All</button>
+        <button onClick={refreshPage}>Refresh</button>
+
+      </section>
+
+
+  </main>
+
+  );
 }
 
 export default App;
